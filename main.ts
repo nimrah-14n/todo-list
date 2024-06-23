a954ee98ca901256579c5a26a6bf356970720b60
Nimrah Hussain <160315958+nimrah-14n@users.noreply.github.com>
2024-06-23 10:48:07 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todoos =[];
+let condition = true;
+while(condition){
+let AddTask = await inquirer.prompt(
+     [
+        {
+            name : 'todo',
+            message : "what do you want to add in your too do list?",
+            type : 'input',
+        },
+        {
+           name : 'AddMore',
+           message : "do you want to add more?",
+           type : 'confirm',
+           default : 'false',
+        },
+    ]
+);
+todoos.push(AddTask.todo);
+condition = AddTask.AddMore;
+console.log(todoos)
+};
+console.log("here is your to do list");
